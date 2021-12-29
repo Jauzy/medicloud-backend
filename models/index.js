3c4bc36d1b1913f58d9977eecc4cf38a467021a9
Jauzy <al.zaujy@gmail.com>
2021-12-29 11:41:57 +0700

    logs

--- models/index.js
+++ models/index.js
@@ -5,6 +5,9 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
     host: dbConfig.host,
     dialect: dbConfig.dialect,
+    dialectOptions: {
+        ssl: true
+    },
     operatorsAliases: false,
     logging: false,
     pool: {
